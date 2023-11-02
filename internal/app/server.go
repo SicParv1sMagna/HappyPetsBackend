3e9c8a31e739018cb56b2be6f6467b21f370d050
Mark Grevtsov <markgrevtsov2310@gmail.com>
2023-11-03 01:00:35 +0300

    [HP-9] Removed the OPTIONS request

--- internal/app/server.go
+++ internal/app/server.go
@@ -20,12 +20,6 @@ func (a *Application) StartServer() {
 
 	// Создаем роутинг
 	router := gin.Default()
-	// Обработчик OPTIONS запросов
-	router.OPTIONS("/*path", func(c *gin.Context) {
-		c.JSON(200, gin.H{
-			"message": "options",
-		})
-	})
 	router.Use(cors.New(cors.Config{
 		AllowOrigins:     []string{"http://localhost:5173"}, // List of allowed origins
 		AllowMethods:     []string{"GET", "POST", "PUT", "DELETE"},
