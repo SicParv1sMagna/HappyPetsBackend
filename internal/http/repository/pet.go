fc4e25f3c80055411d1aa110636c24675f891004
Mark Grevtsov <markgrevtsov2310@gmail.com>
2023-11-03 00:00:23 +0300

    [HP-22]PUT UpdatePet method

--- internal/http/repository/pet.go
+++ internal/http/repository/pet.go
@@ -2,6 +2,7 @@ package repository
 
 import (
 	"errors"
+	"fmt"
 
 	"github.com/SicParv1sMagna/HappyPetsBackend/internal/model"
 )
@@ -31,6 +32,21 @@ func (r *Repository) GetPetById(petID uint) (model.Pet, error) {
 func (r *Repository) UpdatePet(pet model.Pet) error {
 	if err := r.db.Model(&model.Pet{}).Where("id = ?", pet.ID).Updates(pet).Error; err != nil {
 		return errors.New("ошибка при обновлении информации о питомце в БД")
+	}
+	return nil
+}
+
+func (r *Repository) GetPetById(petID uint) (model.Pet, error) {
+	var pet model.Pet
+	if err := r.db.First(&pet, petID).Error; err != nil {
+		return model.Pet{}, fmt.Errorf("ошибка при получении питомца из БД: %v", err)
+	}
+	return pet, nil
+}
+
+func (r *Repository) UpdatePet(pet model.Pet) error {
+	if err := r.db.Model(&model.Pet{}).Where("id = ?", pet.ID).Updates(pet).Error; err != nil {
+		return fmt.Errorf("ошибка при обновлении информации о питомце в БД: %v", err)
 	}
 	return nil
 }
